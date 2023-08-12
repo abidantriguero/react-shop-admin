de666ac9513c675653d60388e8a018966ef30e60
Abidan Triguero <abidantriguero@gmail.com>
2023-08-12 14:33:49 -0400

    CLASE 10: Presentación de la API -v1

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,9 +1,14 @@
 const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
 const endPoints = {
     auth: {
-        login: '${API}/users/login',
+        login: '${API}/api/{VERSION}/auth/login',
+        profile: '${API}/api/{VERSION}/auth/profile',
     },
+    products: {
+        getProduct: (id) => '${API}/api/{VERSION}/products/${id}',
+    }
 };
 
 export default endPoints;
